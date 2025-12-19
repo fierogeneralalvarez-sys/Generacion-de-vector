b672bd1f0dbcb2fa553e6ea5ea22136c698e695b
fierogeneralalvarez-sys <fierogeneralalvarez@gmail.com>
2025-12-19 14:15:54 -0600

    Add files via upload

---
+++ Generacion-de-vectores-master/Generacion de vectores/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Generacion_de_vectores
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.GenerarVectores = new System.Windows.Forms.Button();
+            this.listavectores = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // GenerarVectores
+            // 
+            this.GenerarVectores.Location = new System.Drawing.Point(12, 26);
+            this.GenerarVectores.Name = "GenerarVectores";
+            this.GenerarVectores.Size = new System.Drawing.Size(139, 48);
+            this.GenerarVectores.TabIndex = 0;
+            this.GenerarVectores.Text = "Generar";
+            this.GenerarVectores.UseVisualStyleBackColor = true;
+            this.GenerarVectores.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listavectores
+            // 
+            this.listavectores.FormattingEnabled = true;
+            this.listavectores.ItemHeight = 16;
+            this.listavectores.Location = new System.Drawing.Point(226, 49);
+            this.listavectores.Name = "listavectores";
+            this.listavectores.Size = new System.Drawing.Size(169, 132);
+            this.listavectores.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(482, 218);
+            this.Controls.Add(this.listavectores);
+            this.Controls.Add(this.GenerarVectores);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button GenerarVectores;
+        private System.Windows.Forms.ListBox listavectores;
+    }
+}
+
---
+++ Generacion-de-vectores-master/Generacion de vectores/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Generacion_de_vectores
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int[] numeros = new int[5];
+
+            numeros[0] = 23455;
+            numeros[1] = 6567;
+            numeros[2] = 2568;
+            numeros[3] = 4454;
+            numeros[4] = 4444;
+            listavectores.Items.Clear();
+
+            for (int i = 0; i < numeros.Length; i++)
+            {
+
+                listavectores.Items.Add(numeros[i]);
+            }
+        }
+    }
+}
